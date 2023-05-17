a006ca1b349beea29649926730e4331c92a5a038
elangptra <rvnlz17@gmail.com>
2023-05-17 11:42:17 +0700

    Update Praktikum 3 - Menyimpan data REST API

--- app/Http/Controllers/MahasiswaController.php
+++ app/Http/Controllers/MahasiswaController.php
@@ -5,6 +5,7 @@ namespace App\Http\Controllers;
 use App\Http\Resources\MahasiswaResource;
 use App\Models\Mahasiswa;
 use Illuminate\Http\Request;
+use App\Http\Requests\StoreMahasiswaRequests;
 
 class MahasiswaController extends Controller
 {
@@ -34,9 +35,17 @@ class MahasiswaController extends Controller
      * @param  \Illuminate\Http\Request  $request
      * @return \Illuminate\Http\Response
      */
-    public function store(Request $request)
+    public function store(StoreMahasiswaRequests $request)
     {
-        //
+        // return response()->json('hello');
+        return new MahasiswaResource(Mahasiswa::create(
+            [
+                'Nim'=>$request->Nim,
+                'Nama'=>$request->Nama,
+                'Jurusan'=>$request->Jurusan,
+                'No_Handphone'=>$request->No_Handphone,
+                'kelas_id'=>$request->kelas_id,
+        ]));
     }
 
     /**
