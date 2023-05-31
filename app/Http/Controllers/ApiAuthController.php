b7992ec9ddd694f873f562c13201bbce29e1af8e
elangptra <rvnlz17@gmail.com>
2023-05-31 11:06:41 +0700

    Pertemuan 13 - Praktikum 1

---
+++ app/Http/Controllers/ApiAuthController.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use App\Models\User;
+use Illuminate\Support\Facades\Hash;
+use App\Http\Requests\LoginRequest;
+use App\Http\Resources\LoginResource;
+
+class ApiAuthController extends Controller
+{
+    public function login(LoginRequest $request){
+
+        //check apakah data user yang diinput ada
+        $user = User::where('username', $request->username)->first();
+
+        //check password
+        if(!$user || !Hash::check($request->password, $user->password)){
+            return response()->json([
+                'message' => 'user atau password salah'
+            ], 4811);
+        }
+
+        //generate token
+        $token = $user->createToken('token')->plainTextToken;
+
+        return new LoginResource([
+            'message' => 'success login',
+            'user' => $user,
+            'token' => $token,
+        ], 200);
+    }
+}
--- routes/api.php
+++ routes/api.php
@@ -3,6 +3,7 @@
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\MahasiswaController;
+use App\Http\Controllers\ApiAuthController;
 
 /*
 |--------------------------------------------------------------------------
@@ -29,3 +30,5 @@ Route:: get('/hello', function(){
 // });
 
 Route::apiResource('/mahasiswa', MahasiswaController::class);
+
+Route::post('login', [ApiAuthController::class, 'login']);
