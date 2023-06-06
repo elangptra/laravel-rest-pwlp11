f7e1a370a82d3e1eb7c489bffd8c6596adf9ac42
elangptra <rvnlz17@gmail.com>
2023-06-06 20:18:49 +0700

    Pertemuan 13 - Praktikum 2

--- routes/api.php
+++ routes/api.php
@@ -32,3 +32,8 @@ Route:: get('/hello', function(){
 Route::apiResource('/mahasiswa', MahasiswaController::class);
 
 Route::post('login', [ApiAuthController::class, 'login']);
+
+Route::middleware('auth:sanctum')->group(function() {
+    Route::apiResource('/mahasiswa', MahasiswaController::class);
+    Route::get('/logout', [ApiAuthController::class, 'logout']);
+});
